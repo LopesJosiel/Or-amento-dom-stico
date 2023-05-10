d505968079513d6223806e55459d5ab550cc95fb
LopesJosiel <josielslopes@gmail.com>
2023-05-09 21:27:06 -0300

    Organizando as estruturas para interação com o banco de cados

--- ProjetoOrcamentoDomestico.py
+++ ProjetoOrcamentoDomestico.py
@@ -11,3 +11,34 @@ while True:
     print (lista1) #erro pois float não é iterável
     
 
+#para criar conexão com o banco de dados
+
+import mysql.connector
+#criando o tratamento com o banco de dados
+
+mydb = mysql.connector.connect(
+  host="localhost",
+  user="yourusername",
+  password="yourpassword"
+)
+mycursor = mydb.cursor()
+
+#inserir receitas
+var_data = input('Escolha a data')
+var_valor1 = input('qual o valor da receita')
+var_valor = float (var_valor1)
+var_descricao = input ('Qual a descrição da receita?')
+
+com_insert_rec = f'INSERT INTO receitas (data, valor, descrição) VALUES ({var_data}, {var_valor}, "{var_descricao}")'
+cursor.execute (com_insert_rec)
+conexao.commit ()
+#inserir despesas
+
+
+comando = 'comando  sql'
+cursor.execute (comando)
+conexao.commit () #quando edito o banco de dados, não preciso quando leio
+resultado = cursor.fetchall () #ler o banco de dados
+
+cursor.close () # fechando cursor quanto for executado
+conexao.close() #fechando conexão futura ao banco de dados
