99a8301f7a94af54af78cbf5a70cff089606f3e7
LopesJosiel <josielslopes@gmail.com>
2023-05-10 22:41:20 -0300

    inclusão de receita e despesa no BD ok

--- ProjetoOrcamentoDomestico.py
+++ ProjetoOrcamentoDomestico.py
@@ -1,44 +1,60 @@
 # iniciando o projeto
 print ('Bem-vindo ao mundo das finanças organizadas! Lembre-se de que os números não mentem')
 #criando um input para receber as receitas
-while True:
-    receita_entrada = input('Digite valor e data da receita')
-    receitas = float (receita_entrada)
-    lista1 = []
-    #criar um loop para armazenar cada entrada de receita
-    for receita in receitas:
-        lista1.append (receitas)
-    print (lista1) #erro pois float não é iterável
-    
-
-#para criar conexão com o banco de dados
-
 import mysql.connector
-#criando o tratamento com o banco de dados
-
-mydb = mysql.connector.connect(
+orcamentodemestico = mysql.connector.connect(
   host="localhost",
-  user="yourusername",
-  password="yourpassword"
+  user="root",
+  password="1035308",
+  database="orcamentodomestico"
 )
-mycursor = mydb.cursor()
+mycursor = orcamentodemestico.cursor()
+entrada = input ('receita[r], despesa [d], saldo[s], fechar [f]') #entrada que definirá o caminho a ser seguido pelo programa
+#criando o tratamento com o banco de dados
+while entrada == 'r': #input da receita realizado com sucesso
+    var_data = input('Escolha a data')
+    var_valor1 = input('qual o valor da receita')
+    var_valor = float (var_valor1)
+    var_descricao = input ('Qual a descrição da receita?')
+    com_insert_rec = f'INSERT INTO receita (data, valor, descricao) VALUES ({var_data}, {var_valor}, "{var_descricao}")'
+    mycursor.execute (com_insert_rec)
+    orcamentodemestico.commit ()
+    mycursor.close () # fechando cursor quanto for executado
+    orcamentodemestico.close() #fechando conexão futura ao banco de dados 
+
+
+mycursor = orcamentodemestico.cursor()
+#criando o tratamento com o banco de dados
+while entrada == 'd': #input da receita realizado com sucesso
+    var_data = input('Escolha a data')
+    var_valor1 = input('qual o valor da despesa')
+    var_valor = float (var_valor1)
+    var_descricao = input ('Qual a descrição da despesa?')
+    com_insert_rec = f'INSERT INTO despesa (data, valor, descricao) VALUES ({var_data}, {var_valor}, "{var_descricao}")'
+    mycursor.execute (com_insert_rec)
+    orcamentodemestico.commit ()
+    mycursor.close () # fechando cursor quanto for executado
+    orcamentodemestico.close() #fechando conexão futura ao banco de dados 
+
+
+#mycursor = mydb.cursor()
 
 #inserir receitas
-var_data = input('Escolha a data')
-var_valor1 = input('qual o valor da receita')
-var_valor = float (var_valor1)
-var_descricao = input ('Qual a descrição da receita?')
+#var_data = input('Escolha a data')
+#var_valor1 = input('qual o valor da receita')
+#var_valor = float (var_valor1)
+#var_descricao = input ('Qual a descrição da receita?')
 
-com_insert_rec = f'INSERT INTO receitas (data, valor, descrição) VALUES ({var_data}, {var_valor}, "{var_descricao}")'
-cursor.execute (com_insert_rec)
-conexao.commit ()
+#com_insert_rec = f'INSERT INTO receitas (data, valor, descrição) VALUES ({var_data}, {var_valor}, "{var_descricao}")'
+#cursor.execute (com_insert_rec)
+#conexao.commit ()
 #inserir despesas
 
 
-comando = 'comando  sql'
-cursor.execute (comando)
-conexao.commit () #quando edito o banco de dados, não preciso quando leio
-resultado = cursor.fetchall () #ler o banco de dados
-
-cursor.close () # fechando cursor quanto for executado
-conexao.close() #fechando conexão futura ao banco de dados
+#comando = 'comando  sql'
+#cursor.execute (comando)
+#conexao.commit () #quando edito o banco de dados, não preciso quando leio
+#resultado = cursor.fetchall () #ler o banco de dados
+#
+#cursor.close () # fechando cursor quanto for executado
+#conexao.close() #fechando conexão futura ao banco de dados
--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # Orcamento-domestico
 O objetivo deste projeto é criar um programa que armazena receitas e despesas do usuário e mostra o saldo para o usuário final.
+PARA UM BOM USO DO mySQL:
+-colocar data no formato AAAMMDD (tudo junto e sem espaçamento)
+##### progredindo bem!
 
